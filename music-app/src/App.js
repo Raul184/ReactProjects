3872ff4f513ae4087793ee2f7ded7daef7d914cd
raul <snash@tutanota.com>
2019-09-03 13:16:49 +0200

    fetch api method added for singer request and top-tracks

--- music-app/src/App.js
+++ music-app/src/App.js
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React , {useState} from 'react';
 import './App.css';
 
 function App() {
+//hooks
+  const [artist , setArtist] = useState('');
+  const [tracks , setTracks] = useState([]);
+  
+  //ON SUBMIT
+  //1. Search for an Artist
+  const searchArtist = async () => {
+    try {
+      const request = await fetch(`https://spotify-api-wrapper.appspot.com/artist/${artist}`);
+      const response = await request.json();
+      
+      //find artists
+      if(response.artists.total > 0){
+
+        // 1 save artist 
+        const artistSearch = response.artists.items[0];
+        setArtist(artistSearch);
+        // 2 get top tracks
+        const tracksReq = await fetch(`https://spotify-api-wrapper.appspot.com/artist/${artistSearch.id}/top-tracks`);
+        const trackRes = await tracksReq.json();
+
+        setTracks(trackRes.tracks);
+      }  
+    } 
+    catch (error) {
+      console.log(error)
+    }
+  }
+
+  //2. Enter ==> Search an Artist
+  const handleKeyPress = async (e) => {
+    if(e.key === 'Enter'){
+      searchArtist();
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Music Box</h2>
+      <input 
+        type='text' 
+        name='name'
+        value={artist}
+        onChange={(e) => setArtist(e.target.value)}
+        onKeyPress={handleKeyPress} 
+        placeholder='Search for your favorite artist'/>
+      <button onClick={searchArtist}>Search</button>
     </div>
   );
 }

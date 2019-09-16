c126057abcf5e84bed608f963ce2ed22307414e7
raul <snash@tutanota.com>
2019-09-16 07:28:22 +0200

    pages and main components added

---
+++ movies/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+//Routing 
+import { Switch, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      Hello world
+    </div>
+  );
+}
+
+export default App;
